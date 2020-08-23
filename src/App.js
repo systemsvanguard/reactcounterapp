d757f5d22e4bcd50756eaabf717c9b9d72643d4b
systemsvanguard <ryanhunterjnr@gmail.com>
2020-08-23 03:28:02 -0400

    Updated Format

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import TheCount from './app/images/TheCount.jpg';
-// import { yieldExpression } from '@babel/types';
 
 
 function App() {
-	const initialState = 17;
+	const initialState = 17; /* sets the same number as the picture */
 	const [ counter, setCount] = React.useState(initialState);
 
 	const addOne = ()=> {
@@ -43,35 +42,34 @@ function App() {
             <h1 className="title has-text-link has-text-centered">React Counter App with Hooks</h1>
             <p className="subtitle has-text-danger-dark has-text-weight-bold has-text-centered">Our Counter is now : <span className="is-italic is-size-3 has-text-danger">{counter}</span>. </p>
 
-						<p className="has-text-centered">
-							<button className="button is-danger is-outlined" onClick={minusThree}>
-							<span className="icon is-small"><i className="fas fa-minus"></i></span>
-							<span> Minus 3 </span>
-							</button>
-							<button className="button is-danger" onClick={minusOne}>
-							<span className="icon is-small"><i className="fas fa-minus"></i></span>
-							<span> Minus 1 </span>
-							</button>
+			<p className="has-text-centered">
+				<button className="button is-danger is-outlined" onClick={minusThree}>
+				<span className="icon is-small"><i className="fas fa-minus"></i></span>
+				<span> Minus 3 </span>
+				</button>
+				<button className="button is-danger" onClick={minusOne}>
+				<span className="icon is-small"><i className="fas fa-minus"></i></span>
+				<span> Minus 1 </span>
+				</button>
 
-							<button className="button is-link is-medium is-inverted" onClick={resetCount}>
-							<span className="icon is-small"><i className="fas fa-redo"></i></span>
-							<span> Reset </span>
-							</button>
+				<button className="button is-link is-medium is-inverted" onClick={resetCount}>
+				<span className="icon is-small"><i className="fas fa-redo"></i></span>
+				<span> Reset </span>
+				</button>
 
-							<button className="button is-link" onClick={addOne}>
-							<span className="icon is-small"><i className="fas fa-plus"></i></span>
-							<span> Add 1 </span>
-							</button>
-							<button className="button is-link is-outlined" onClick={addFive}>
-							<span className="icon is-small"><i className="fas fa-plus"></i></span>
-							<span> Add 5 </span>
-							</button>
-							<button className="button is-success" onClick={timesTwo}>
-							<span className="icon is-small"><i className="fas fa-times"></i></span>
-							<span> Times 2 </span>
-							</button>
-						</p>
-						<br />
+				<button className="button is-link" onClick={addOne}>
+				<span className="icon is-small"><i className="fas fa-plus"></i></span>
+				<span> Add 1 </span>
+				</button>
+				<button className="button is-link is-outlined" onClick={addFive}>
+				<span className="icon is-small"><i className="fas fa-plus"></i></span>
+				<span> Add 5 </span>
+				</button>
+				<button className="button is-success" onClick={timesTwo}>
+				<span className="icon is-small"><i className="fas fa-times"></i></span>
+				<span> Times 2 </span>
+				</button>
+			</p><br />
             <img src={TheCount} className="responsiveImages" alt="The Count Demonstrates Counting" title="The Count Demonstrates Counting" />
           </div>
       </section>
